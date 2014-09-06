3348e05af2a052d1efea64189a0008c4802b1976
lpenguin <lpenguin@yandex.ru>
2014-09-06 17:57:27 +0400

    init function

---
+++ init.lua
@@ -0,0 +1,15 @@
+function download(url, dest)
+    local gist_url = url
+    local request = http.get( gist_url )
+    local response = request.readAll()
+    request.close()
+    local file = fs.open( dest, "w" )
+    file.write( response )
+    file.close()
+end
+
+local root = "https://raw.githubusercontent.com/lpenguin/minecraft-turtle/master/"
+
+download(root.."utils.lua", "utils")
+download(root.."path.lua", "path")
+download(root.."move-mc.lua", "move")
